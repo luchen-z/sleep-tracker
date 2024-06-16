47ba573d49beb4804eeb084701023309dd6227d6
Xulai Cao <caoxulaistudy@gmail.com>
2024-06-15 23:39:32 -0400

    Remove duration in the data storage

--- utils.py
+++ utils.py
@@ -8,3 +8,11 @@ def sort_nested_dict(d):
     for key in sorted(d):
         sorted_dict[key] = sort_nested_dict(d[key])
     return dict(sorted_dict)
+
+
+def get_duration(sleep_time, wake_time):
+    # assuming a normal sleep duration < 24 hours
+    sleep_duration = wake_time - sleep_time
+    duration_in_hours = sleep_duration.total_seconds() / 3600
+    duration_formatted = f"{duration_in_hours:.2f}" + " hours"
+    return duration_formatted
